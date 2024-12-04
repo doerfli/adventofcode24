ff7eb63e25070a0d99633183adbf2ceee980064e
Marc Doerflinger <marc@doerf.li>
2024-12-04 13:43:04 +0000

    cleanup

--- src/04/main2.rs
+++ src/04/main2.rs
@@ -8,10 +8,10 @@ fn main() {
 
     let mut found_mas = 0;
 
-    for y in 0..len_y {
-        for x in 0..len_x {
+    for y in 1..len_y - 1 {
+        for x in 1 .. len_x - 1 {
             if matrix[y][x] == 'A' {
-                // extract the X as text and match against valid MAS x MAX variants
+                // extract the X chars as text and match against valid MAS x MAS variants
                 let diagonals = get_diagonals(&matrix, x, y, len_x, len_y);
                 println!("Diagonals: {}", diagonals);
                 if (diagonals == "MASMAS") {
@@ -32,10 +32,6 @@ fn main() {
 
 fn get_diagonals(matrix: &Vec<Vec<char>>, x: usize, y: usize, len_x: usize, len_y: usize) -> String {
     let mut diagonals = "".to_owned();
-    if x < 1 || x >= len_x - 1 || y < 1 || y >= len_y - 1 {
-        return "".to_owned();
-    }
-    
     diagonals.push(matrix[y-1][x-1]);
     diagonals.push(matrix[y][x]);
     diagonals.push(matrix[y+1][x+1]);
@@ -43,24 +39,6 @@ fn get_diagonals(matrix: &Vec<Vec<char>>, x: usize, y: usize, len_x: usize, len_
     diagonals.push(matrix[y][x]);
     diagonals.push(matrix[y+1][x-1]);
     return diagonals;
-}
-
-fn is_valid(word: &str, matrix: &Vec<Vec<char>>, start_x: usize, start_y: usize, dx: i32, dy: i32, len_x: usize, len_y: usize) -> bool {
-    let len = word.len();
-    let mut x = start_x as i32;
-    let mut y = start_y as i32;
-
-    for i in 0..len {
-        if x < 0 || x >= len_x as i32 || y < 0 || y >= len_y as i32 {
-            return false;
-        }
-        if matrix[y as usize][x as usize] != word.chars().nth(i).unwrap() {
-            return false;
-        }
-        x += dx;
-        y += dy;
-    }
-    return true;
 }
 
 fn get_input() -> Vec<Vec<char>> {
